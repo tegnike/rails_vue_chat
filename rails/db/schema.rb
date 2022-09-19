6e817bf5f7184c89b84c4dc3e457f40cf97700f1
tegnike <o3nike.teg.14@gmail.com>
2022-09-19 21:38:22 +0200

    メッセージ一覧の取得

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_145142) do
+ActiveRecord::Schema.define(version: 2022_09_19_193151) do
+
+  create_table "messages", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "provider", default: "email", null: false
--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+## ======= 👇 ここから追加する =======
+3.times do |number|
+  Message.create(content: "#{number}番目のメッセージです！", user_id: User.first.id)
+  puts "#{number}番目のメッセージを作成しました"
+end
+
+puts "メッセージの作成が完了しました"
