088b398ef4940a14613ac7b583f275c78509b073
tegnike <o3nike.teg.14@gmail.com>
2022-09-21 00:10:40 +0200

    rails連携/チャット機能実装

--- rails/app/channels/room_channel.rb
+++ rails/app/channels/room_channel.rb
@@ -1,6 +1,6 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'room_channel'
+    stream_from "room_channel_#{params[:room]}"
   end
 
   def unsubscribed
@@ -11,7 +11,7 @@ class RoomChannel < ApplicationCable::Channel
     user = User.find_by(email: data['email'])
     
     if message = Message.create(content: data['message'], user_id: user.id)
-      ActionCable.server.broadcast 'room_channel', { message: data['message'], name: user.name, created_at: message.created_at }
+      ActionCable.server.broadcast "room_channel_#{data['email']}", { message: data['message'], name: user.name, created_at: message.created_at }
     end
   end
 end
--- vue/src/views/ChatroomPage.vue
+++ vue/src/views/ChatroomPage.vue
@@ -2,16 +2,19 @@
   <div class="container">
     <NavbarComponent />
     <ChatWindow :messages="messages" />
+    <NewChatForm @connectCable="connectCable" />
   </div>
 </template>
 
 <script>
 import NavbarComponent from '../components/NavbarComponent.vue'
 import ChatWindow from '../components/ChatWindow.vue'
+import NewChatForm from '../components/NewChatForm.vue'
 import axios from 'axios'
+import ActionCable from 'actioncable'
 
 export default {
-  components: { NavbarComponent, ChatWindow },
+  components: { NavbarComponent, ChatWindow, NewChatForm },
   data () {
     return {
       messages: []
@@ -36,10 +39,28 @@ export default {
       } catch (err) {
         console.log(err)
       }
+    },
+    connectCable (message) {
+      this.messageChannel.perform('receive', {
+        message: message,
+        email: window.localStorage.getItem('uid')
+      })
     }
   },
   mounted() {
-    this.getMessages()
+    const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+    this.room = localStorage.getItem('uid')
+    this.messageChannel = cable.subscriptions.create({ channel: 'RoomChannel', room: this.room }, {
+      connected: () => {
+        this.getMessages()
+      },
+      received: () => {
+        this.getMessages()
+      }
+    })
+  },
+  beforeUnmount () {
+    this.messageChannel.unsubscribe()
   }
 }
 </script>
