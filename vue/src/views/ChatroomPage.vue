3a20a52a21c66e4c15e15dc0834763656a2f6e75
tegnike <o3nike.teg.14@gmail.com>
2022-09-20 23:01:27 +0200

    rails連携/メッセージ一覧表示

--- vue/src/views/ChatroomPage.vue
+++ vue/src/views/ChatroomPage.vue
@@ -1,14 +1,46 @@
 <template>
   <div class="container">
     <NavbarComponent />
+    <ChatWindow :messages="messages" />
   </div>
 </template>
 
 <script>
 import NavbarComponent from '../components/NavbarComponent.vue'
+import ChatWindow from '../components/ChatWindow.vue'
+import axios from 'axios'
 
 export default {
-  components: { NavbarComponent }
+  components: { NavbarComponent, ChatWindow },
+  data () {
+    return {
+      messages: []
+    }
+  },
+  methods: {
+    async getMessages () {
+      try {
+        const res = await axios.get('http://localhost:3000/messages', {
+          headers: {
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client: window.localStorage.getItem('client')
+          }
+        })
+
+        if (!res) {
+          throw new Error('メッセージ一覧を取得できませんでした')
+        }
+
+        this.messages = res.data
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+  mounted() {
+    this.getMessages()
+  }
 }
 </script>
 
