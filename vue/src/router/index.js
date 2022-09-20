2127fbf1a994bcf9c11df94b0aaa76fc941be3b3
tegnike <o3nike.teg.14@gmail.com>
2022-09-20 21:47:13 +0200

    rails連携/各画面の認証アクセス制御

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,17 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/WelcomePage'
 import Chatroom from '../views/ChatroomPage'
+import useValidate from '../auth/validate'
+
+const { error, validate } = useValidate()
+
+const requireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid || !client || !accessToken) {
+    console.log('ログインしていません')
+    next({ name: 'Welcome' })
+    return
+  }
+
+  await validate()
+
+  if (error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
+}
+
+const noRequireAuth = async (to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('access-token')
+
+  if (!uid && !client && !accessToken) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if (!error.value) {
+    next({ name: 'Chatroom' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'Welcome',
-    component: Welcome
+    component: Welcome,
+    beforeEnter: noRequireAuth
   },
   {
     path: '/chatroom',
     name: 'Chatroom',
-    component: Chatroom
+    component: Chatroom,
+    beforeEnter: requireAuth
   }
 ]
 
