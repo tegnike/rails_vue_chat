2191cbf216277314eb7b28029968faa3db064786
tegnike <o3nike.teg.14@gmail.com>
2022-09-23 17:46:55 +0200

    rails g graphql

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
