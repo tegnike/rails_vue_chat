a036c0db8c410b92df09a236233c32c31b0535fd
tegnike <o3nike.teg.14@gmail.com>
2022-09-23 21:57:03 +0200

    graphql/devise追加&ログイン確認

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations: 'auth/registrations'
-  }
+  # mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+  #   registrations: 'auth/registrations'
+  # }
+  mount_graphql_devise_for User, at: 'graphql_auth'
+  as :user do
+    # Define routes for User within this block.
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :messages, only: :index do
