ba2d3fd4af261f46a4591f34421da03cde0add87
tegnike <o3nike.teg.14@gmail.com>
2022-09-23 16:24:44 +0200

    rails連携/いいね機能実装

--- vue/src/views/ChatroomPage.vue
+++ vue/src/views/ChatroomPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <NavbarComponent />
-    <ChatWindow :messages="messages" />
+    <ChatWindow @connectCable="connectCable" :messages="messages" />
     <NewChatForm @connectCable="connectCable" />
   </div>
 </template>
